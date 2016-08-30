eee9c6d6e0b2804895c4610f8fbb4de36cccbd99
florinpopescu <florin.popescu0110@gmail.com>
2016-08-30 19:34:04 +0300

    Added basic end to end functionality.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,25 @@ var Demo = React.createClass({
         <h1>Angular adapter for the Froala WYSIWYG editor</h1>
         <div className="sample">
           <h2>Sample 1: Inline Edit</h2>
-          <FroalaEditor />
+          <FroalaEditor
+            tag='textarea'
+            config={{
+              placeholderText: 'Add a Title',
+              charCounterCount: false,
+              toolbarInline: true,
+              events: {
+                'froalaEditor.initialized': function() {
+                  console.log('initialized');
+                }
+              }
+            }}
+          />
+        </div>
+        <div className="sample">
+          <h2>Sample2: Full Editor</h2>
+          <FroalaEditor/>
+          <h4>Rendered Content:</h4>
+          <div></div>
         </div>
       </div>
     );
