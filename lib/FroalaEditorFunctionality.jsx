a695102e03d6ad622c435765fda25e0aadccf8a0
Stefan Neculai <stefan.neculai@gmail.com>
2019-05-23 13:28:00 +0300

    Fix typo.

--- lib/FroalaEditorFunctionality.jsx
+++ lib/FroalaEditorFunctionality.jsx
@@ -237,7 +237,7 @@ export default class FroalaEditorFunctionality extends React.Component {
       return;
     }
 
-    if (eventName == 'initialied') {
+    if (eventName == 'initialized') {
       if (!this._initEvents) this._initEvents = [];
       this._initEvents.push(callback);
     }
