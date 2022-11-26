a59c32fad6715c5f0f6fd15a562ec110b55fea9f
Gangadhar <gangadhar.k@CELSYS217-BLR.CELSYSWTC.IN>
2022-11-26 14:14:36 +0530

    deploy-yes

--- push-image-to-nexus.sh
+++ push-image-to-nexus.sh
@@ -2,16 +2,15 @@
 export BRANCH_NAME=`echo "${TRAVIS_BRANCH}" | tr '[:upper:]' '[:lower:]'`
 case "${BRANCH_NAME}" in
         dev*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD" ;;
-	ao-dev*)echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
+	       ao-dev*)echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
         qa*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-	qe*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-	rc*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
-	release-master*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
+	       qe*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
+	       rc*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
+	       release-master*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD"  ;;
         ft*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI" ;;
         bf*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI" ;;
         *) echo "Not a valid branch name for CI/CD" && exit -1;;
 esac
-
 echo $TRAVIS_BRANCH
 echo ${DEPLOYMENT_SERVER}
 export SHORT_COMMIT=`git rev-parse --short=7 ${TRAVIS_COMMIT}`
@@ -28,8 +27,8 @@ cat package.json
 docker build -t  ${IMAGE_NAME}:${SHORT_COMMIT} --build-arg PackageName=${PACKAGE_NAME} --build-arg PackageVersion=${PACKAGE_VERSION} --build-arg NexusUser=${NEXUS_USER} --build-arg NexusPassword=${NEXUS_USER_PWD} .
 sleep 3
 docker image ls 
-echo "uploading to nexus ${PACKAGE_NAME}, if not a new PR"
 if [ ${TRAVIS_PULL_REQUEST} != "false" ];  then echo "Not publishing a pull request !!!" && exit 0; fi
+echo "uploading to nexus" ${PACKAGE_NAME}
 docker login -u ${NEXUS_USER} -p ${NEXUS_USER_PWD} ${NEXUS_CR_TOOLS_URL}
 docker tag  ${IMAGE_NAME}:${SHORT_COMMIT} ${NEXUS_CR_TOOLS_URL}/froala-${IMAGE_NAME}:${PACKAGE_VERSION}
 docker push ${NEXUS_CR_TOOLS_URL}/froala-${IMAGE_NAME}:${PACKAGE_VERSION}
