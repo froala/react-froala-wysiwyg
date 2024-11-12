59e94b6ec02f14086314e4bcb271a3b422a916ba
AndriiMysko <andrii.mysko@devtactics.net>
2024-11-12 14:09:41 +0200

    Fix demo

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -41,7 +41,12 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader'
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: false
+            }
+          }
         ]
       },
       {
