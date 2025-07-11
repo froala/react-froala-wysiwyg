f159f0e6f290197c85f6b1a5eb8a4024ed66e5b1
harasunu-narayan <60644555+harasunu-narayan@users.noreply.github.com>
2025-07-11 20:38:54 +0530

    Update basic.jsx
    [deploy-yes]

--- demo/src/basic.jsx
+++ demo/src/basic.jsx
@@ -12,6 +12,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('editor'));
 root.render(<FroalaEditor tag='textarea' config={{
   pluginsEnabled: ['align', 'link'],
+  key: 'YNB3fH3C10C7B7B5D2G2C-8evjgkiB5cejJ-7oA8sD-13F-11oF4I4B3C11A3B5E4B2A3B3==',
   language: 'ro',
   events: {
     initialized: function (e) {
