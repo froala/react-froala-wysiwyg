f82e9d8be11b43b2fdf8f9981b4cf919af3cc795
harasunu-narayan <harasunu.narayan@celestialsys.com>
2025-07-23 16:39:26 +0530

    Revert the demo changes

--- demo/src/full_editor.jsx
+++ demo/src/full_editor.jsx
@@ -2,13 +2,13 @@
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 import 'froala-editor/css/froala_style.css';
 import 'file-loader?name=[name].[ext]!./full_editor.html';
+import 'froala-editor/js/plugins.pkgd.min.js';
 
 import FroalaEditor from 'react-froala-wysiwyg';
 import FroalaEditorView from 'react-froala-wysiwyg/FroalaEditorView';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import 'froala-editor/js/plugins.pkgd.min.js';
 // Render Froala Editor component.
 class EditorComponent extends React.Component {
   constructor() {
@@ -30,13 +30,8 @@ class EditorComponent extends React.Component {
   render () {
     return(
       <div className="sample">
-        <h2>Unlicensed Froala</h2>
-        <FroalaEditor />
-        <h2>Licensed Froala</h2>
-        <FroalaEditor config={{key: "UBB7jD5D5B3F4A2B10C10bHIMFI1EWBXIJe1BZLZFd1d1MXQLjC10D7D5A3B2A3E4E2C2C4==", fontSizeDefaultSelection: 18, fontFamilyDefaultSelection: 'Tahoma'}} />
         <h2>Full Featured</h2>
         <FroalaEditor
-          config={{key: "YNB3fH3C10C7B7B5D2G2C-8evjgkiB5cejJ-7oA8sD-13F-11oF4I4B3C11A3B5E4B2A3B3==", fontSizeDefaultSelection: 18, fontFamilyDefaultSelection: 'Tahoma'}}
           model={this.state.content}
           onModelChange={this.handleModelChange}
         />
